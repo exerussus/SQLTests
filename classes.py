21bf99bde42fd97243e5a0a246afc185b37dc2dd
exerussus <solovyov.production@gmail.com>
2022-04-24 19:54:25 +0300

    Added Gui class.

--- classes.py
+++ classes.py
@@ -1,3 +1,21 @@
+from kivy.app import App
+
+
+class Gui(App):
+
+    def build(self):
+
+        from kivy.uix.boxlayout import BoxLayout
+        from kivy.uix.label import Label
+        from kivy.uix.button import Button
+        from kivy.config import Config
+
+        bl = BoxLayout()
+        self.lbl = Label(text='SELECT LOGIN OPTION', font_size=40)
+        bl.add_widget(self.lbl)
+        bl.add_widget(Button(text='LOG IN'))
+        bl.add_widget(Button(text='SIGN UP'))
+        return bl
 
 
 class Identification:
